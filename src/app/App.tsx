d3a0c0a13cc14c4295b010423c43a9eccf2bd892
akb2 <akb2-online@ya.ru>
2025-08-15 22:29:03 +0300

    Test: Убрал лоудер

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-const Fallback = () => <div>Загрузка…</div>;
-const App = () => <RouterProvider router={router} fallbackElement={<Fallback />} />;
+const App = () => <RouterProvider router={router} />;
 
 export default App;
