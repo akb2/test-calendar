2ede6d0d153b0270beda8793c4b952c916221bc8
akb2 <akb2-online@ya.ru>
2025-08-15 20:57:48 +0300

    Test: Сделал первую страницу

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app/App';
+import reportWebVitals from './shared/lib/reportWebVitals';
+import './shared/styles/index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,3 @@
+export const HomePage = () => (
+  <div>123</div>
+);
