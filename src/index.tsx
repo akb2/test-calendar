bf083f04597f13e9ce0480e69cba0e8865f8f230
akb2 <akb2-online@ya.ru>
2025-08-19 21:11:01 +0300

    Test: Подключил Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import App from "#app/App";
 import reportWebVitals from "#libs/reportWebVitals";
+import { store } from "#store";
 import "#styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
