07bfb6ae11ac8314d6e0e9a15b92361977f40622
akb2 <akb2-online@ya.ru>
2025-08-21 01:24:32 +0300

    Test: Удалил подвал

--- testcalendar.client/src/app/components/layouts/DefaultLayout.tsx
+++ testcalendar.client/src/app/components/layouts/DefaultLayout.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../base/Header/Header";
-import { Container } from "../base/Container/Container";
 
 export const DefaultLayout = () => (
   <div>
@@ -10,9 +9,5 @@ export const DefaultLayout = () => (
     <Suspense fallback={<div> Загрузка...</div>}>
       <Outlet />
     </Suspense>
-
-    <footer>
-      <Container>Footer</Container>
-    </footer>
   </div>
 );
