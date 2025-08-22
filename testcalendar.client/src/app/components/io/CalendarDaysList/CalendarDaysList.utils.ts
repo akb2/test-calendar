4e0c670a4b1a518ce179d5c3bba18c2c7df7b61d
akb2 <akb2-online@ya.ru>
2025-08-22 23:23:37 +0300

    Test: Отметка дл текущего дня

--- testcalendar.client/src/app/components/io/CalendarDaysList/CalendarDaysList.utils.ts
+++ testcalendar.client/src/app/components/io/CalendarDaysList/CalendarDaysList.utils.ts
@@ -4,27 +4,43 @@ import type { MonthDaysListItem } from "./CalendarDaysList.types";
 
 const DaysInMonth = (month: number, year: number) =>
   new Date(year, month + 1, 0).getDate();
+
 const BeforeMonth = (month: number) => (month > 0 ? month - 1 : 11);
+
+const AfterMonth = (month: number) => (month < 11 ? month + 1 : 0);
+
 const BeforeMonthYear = (month: number, year: number) =>
-  month > 0 ? year : year - 1;
+  BeforeMonth(month) > 0 ? year : year - 1;
+
+const AfterMonthYear = (month: number, year: number) =>
+  AfterMonth(month) < 11 ? year : year + 1;
+
+const IsCurrentDay = (date: Date, day: number, month: number, year: number) => {
+  const currentDay = date.getDate();
+  const currentMonth = date.getMonth();
+  const currentYear = date.getFullYear();
+
+  return day === currentDay && month === currentMonth && year === currentYear;
+};
 
 export const GetMonthDays = (
   mixedMonth?: number,
   mixedYear?: number,
 ): MonthDaysListItem[] => {
   const date = CurrentDateTimer(UpdateInterval);
-  const currentDay = date.getDate();
-  const currentMonth = date.getMonth();
-  const currentYear = date.getFullYear();
-  const month = mixedMonth ?? currentMonth;
-  const year = mixedYear ?? currentYear;
-  const isCurrentPeriod = month === currentMonth && year === currentYear;
-  const maxDay = DaysInMonth(month, year);
-  const beforeMaxDay = DaysInMonth(
-    BeforeMonth(month),
-    BeforeMonthYear(month, year),
-  );
-  const beforeDaysCount = new Date(year, month, 1).getDay() - 1;
+
+  const neededMonth = mixedMonth ?? date.getMonth();
+  const neededYear = mixedYear ?? date.getFullYear();
+
+  const beforeMonth = BeforeMonth(neededMonth);
+  const afterMonth = AfterMonth(neededMonth);
+  const beforeMonthYear = BeforeMonthYear(neededMonth, neededYear);
+  const afterMonthYear = AfterMonthYear(neededMonth, neededYear);
+
+  const beforeMaxDay = DaysInMonth(beforeMonth, beforeMonthYear);
+
+  const maxDay = DaysInMonth(neededMonth, neededYear);
+  const beforeDaysCount = new Date(neededYear, neededMonth, 1).getDay() - 1;
   const calcDays = maxDay + beforeDaysCount;
   const afterDaysCount = Math.round(calcDays / 7) * 7 - calcDays;
   const allDays = beforeDaysCount + maxDay + afterDaysCount;
@@ -32,13 +48,26 @@ export const GetMonthDays = (
   return Array.from({ length: allDays }, (_, i) => {
     const position = i - beforeDaysCount + 1;
     const inMonth = position > 0 && position <= maxDay;
-    const day = inMonth
-      ? position
-      : position <= 0
-        ? beforeMaxDay + position
-        : position - maxDay;
-    const isCurrent = inMonth && day === currentDay && isCurrentPeriod;
+    let day = position;
+    let month = neededMonth;
+    let year = neededYear;
 
-    return { day, inMonth, isCurrent };
+    if (!inMonth) {
+      if (position <= 0) {
+        day = beforeMaxDay + position;
+        month = beforeMonth;
+        year = beforeMonthYear;
+      } else {
+        day = position - maxDay;
+        month = afterMonth;
+        year = afterMonthYear;
+      }
+    }
+
+    return {
+      isCurrent: IsCurrentDay(date, day, month, year),
+      day,
+      inMonth,
+    };
   });
 };
